fa0888e2da32209e11cabd168f97ad370f830f54
Manikanta <manikantaanyum966@gmail.com>
2024-10-04 23:05:34 +0530

    practice ytld-core

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,27 @@
 //practice
 
-const express = require("express");
+const fs = require("fs");
 
-const app = express();
 const ytld = require("ytdl-core");
 
-app.use(express.json());
+const link = "https://www.youtube.com/watch?v=2rOnvgFyPgw";
+console.log(link);
 
-app.post("/download", async (req, res) => {
-  const { link } = req.body;
-  console.log(link);
-
+const youtubeDownloader = async () => {
   if (!ytld.validateURL(link)) {
     return res.status(400).json({ msg: "Not valid url" });
   }
 
   const info = await ytld.getBasicInfo(link);
-  const title = info.player_response.videoDetails;
-  res.json({ msg: title });
-});
+  const info1 = await ytld.getInfo(link);
 
-app.listen(3000);
+  const title = info.player_response.videoDetails;
+
+  info1.formats.forEach((i) => {
+    console.log(
+      `${i.qualityLabel} - ${i.container} - ${i.hasAudio} - ${i.hasVideo}`
+    );
+  });
+};
+
+youtubeDownloader();
